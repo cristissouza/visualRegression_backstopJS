76e13b9f7f6665730047815f2f901dd06e7365e9
Cristiane dos Santos Souza <cristiane.souza@concrete.com.br>
2018-10-17 11:56:55 -0300

    Definindo diferentes cenários de uso do backstop.json

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -3,27 +3,45 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_default_Corpo_0_body_0_phone_galaxyS5.png",
-        "test": "../bitmaps_test/20181011-151255/backstop_default_Corpo_0_body_0_phone_galaxyS5.png",
-        "selector": "body",
-        "fileName": "backstop_default_Corpo_0_body_0_phone_galaxyS5.png",
-        "label": "Corpo",
+        "reference": "../bitmaps_reference/backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_0_iphone_8.png",
+        "test": "../bitmaps_test/20181017-115505/backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_0_iphone_8.png",
+        "selector": "ul#social-midia.fh5co-social",
+        "fileName": "backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_0_iphone_8.png",
+        "label": "Multiple_enviroment",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "error": "Reference file not found /Users/cristiane.dos.souza/Documents/VisualRegression-apresentacao/visualRegressionbackstopJS/backstop_data/bitmaps_reference/backstop_default_Corpo_0_body_0_phone_galaxyS5.png"
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "20.77",
+          "analysisTime": 10
+        },
+        "diffImage": "../bitmaps_test/20181017-115505/failed_diff_backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_0_iphone_8.png"
       },
       "status": "fail"
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_default_Corpo_0_body_1_desktop.png",
-        "test": "../bitmaps_test/20181011-151255/backstop_default_Corpo_0_body_1_desktop.png",
-        "selector": "body",
-        "fileName": "backstop_default_Corpo_0_body_1_desktop.png",
-        "label": "Corpo",
+        "reference": "../bitmaps_reference/backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_1_desktop.png",
+        "test": "../bitmaps_test/20181017-115505/backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_1_desktop.png",
+        "selector": "ul#social-midia.fh5co-social",
+        "fileName": "backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_1_desktop.png",
+        "label": "Multiple_enviroment",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "error": "Reference file not found /Users/cristiane.dos.souza/Documents/VisualRegression-apresentacao/visualRegressionbackstopJS/backstop_data/bitmaps_reference/backstop_default_Corpo_0_body_1_desktop.png"
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "17.44",
+          "analysisTime": 11
+        },
+        "diffImage": "../bitmaps_test/20181017-115505/failed_diff_backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_1_desktop.png"
       },
       "status": "fail"
     }
