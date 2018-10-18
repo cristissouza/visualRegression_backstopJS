44043197383952ff4dde21449d7f437893a84c28
Cristiane dos Santos Souza <cristiane.souza@concrete.com.br>
2018-10-18 13:25:50 -0300

    Alteração no arquivo de configuração

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -3,45 +3,29 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_0_iphone_8.png",
-        "test": "../bitmaps_test/20181017-115505/backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_0_iphone_8.png",
-        "selector": "ul#social-midia.fh5co-social",
-        "fileName": "backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_0_iphone_8.png",
-        "label": "Multiple_enviroment",
+        "reference": "../bitmaps_reference/backstop_default_About_me_0_fh5co-bio_0_iphone_8.png",
+        "test": "../bitmaps_test/20181016-123144/backstop_default_About_me_0_fh5co-bio_0_iphone_8.png",
+        "selector": ".fh5co-bio",
+        "fileName": "backstop_default_About_me_0_fh5co-bio_0_iphone_8.png",
+        "label": "About_me",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "20.77",
-          "analysisTime": 10
-        },
-        "diffImage": "../bitmaps_test/20181017-115505/failed_diff_backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_0_iphone_8.png"
+        "engineErrorMsg": "waiting for selector \".fh5co-bio\" failed: timeout 30000ms exceeded",
+        "error": "Reference file not found /Users/cristiane.dos.souza/Documents/VisualRegression-apresentacao/visualRegressionbackstopJS/backstop_data/bitmaps_reference/backstop_default_About_me_0_fh5co-bio_0_iphone_8.png"
       },
       "status": "fail"
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_1_desktop.png",
-        "test": "../bitmaps_test/20181017-115505/backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_1_desktop.png",
-        "selector": "ul#social-midia.fh5co-social",
-        "fileName": "backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_1_desktop.png",
-        "label": "Multiple_enviroment",
+        "reference": "../bitmaps_reference/backstop_default_About_me_0_fh5co-bio_1_desktop.png",
+        "test": "../bitmaps_test/20181016-123144/backstop_default_About_me_0_fh5co-bio_1_desktop.png",
+        "selector": ".fh5co-bio",
+        "fileName": "backstop_default_About_me_0_fh5co-bio_1_desktop.png",
+        "label": "About_me",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "17.44",
-          "analysisTime": 11
-        },
-        "diffImage": "../bitmaps_test/20181017-115505/failed_diff_backstop_default_Multiple_enviroment_0_ulsocial-midiafh5co-social_1_desktop.png"
+        "engineErrorMsg": "waiting for selector \".img-responsive\" failed: timeout 30000ms exceeded",
+        "error": "Reference file not found /Users/cristiane.dos.souza/Documents/VisualRegression-apresentacao/visualRegressionbackstopJS/backstop_data/bitmaps_reference/backstop_default_About_me_0_fh5co-bio_1_desktop.png"
       },
       "status": "fail"
     }
